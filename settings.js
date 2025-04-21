3b48c522de3d99cc48602110920eaf3ab609d9b1
lukkie580 <jonc454588@gmail.com>
2025-04-21 03:04:38 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'lokkie',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "lokkie",
+  botOwner: 'lokkie🥲', // Your name
+  ownerNumber: '2348152690430', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
